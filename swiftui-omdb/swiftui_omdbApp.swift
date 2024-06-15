fd3d840965a74ec6de27f8db9b4abd60dafbb652
taufiqgiri <taufiq.ichwanusofa@ist.id>
2024-06-15 17:09:29 +0700

    Initial Commit

---
+++ swiftui-omdb/swiftui_omdbApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_omdbApp.swift
+//  swiftui-omdb
+//
+//  Created by Taufiq Ichwanusofa on 15/06/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_omdbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
